0433d9de0cd97e9f0e0bc3e08d5db35168570929
Nicolas Byl <nico@nicolas-byl.eu>
2015-08-19 14:35:24 +0200

    added demo application

---
+++ demo/src/main/java/de/codecentric/nbyl/demo/deis/DeisApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric.nbyl.demo.deis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeisApplication.class, args);
+    }
+}
